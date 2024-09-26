b6bcac5c49c0ac8d70a6bf13df7f84cff7478786
StarkOS <starkos.git@gmail.com>
2024-09-26 21:08:47 +0530

    added more properties from matrixx

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -11,8 +11,12 @@ ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.crdroid.display.version=$(LINEAGE_DISPLAY_VERSION) \
     ro.crdroid.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
     ro.modversion=$(CR_VERSION) \
-    ro.crdroidlegal.url=https://crdroid.net/legal.php
+    ro.matrixx.build.variant=$(STARK_BUILD_VARIANT) \
+    ro.matrixx.maintainer=$(STARK_MAINTAINER) \
+    ro.matrixx.release.type=$(STARK_BUILD_TYPE)
 
+
+    
 # LineageOS Platform SDK Version
 ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.lineage.build.version.plat.sdk=$(LINEAGE_PLATFORM_SDK_VERSION)
